cbd54f42ae53e7eab2ee0d51934c0ef8d6ecc317
mohit2494 <mohit2494@gmail.com>
2020-02-12 23:03:56 -0500

    basic driver

--- BigQ.cc
+++ BigQ.cc
@@ -1,10 +1,47 @@
 #include "BigQ.h"
+#include <vector>
 
 BigQ :: BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen) {
+	
+	Record tRec;
+	Page tPage;
+	Run tRun(runlen);
+	
 	// read data from in pipe sort them into runlen pages
+	while(in.Remove(&tRec)) {
+		if(!tPage.Append(&tRec)) {
+			if (tRun.checkRunFull()) {
+				tRun.sortRun();
+				// TODO:: write run to file
+				tRun.clearPages();
+			}
+			tRun.AddPage(tPage);
+			tPage.EmptyItOut();
+			tPage.Append(&tRec);
+		}
+	}
+	
+	if(tPage.getNumRecs()>0) {
+		if (tRun.checkRunFull()) {
+			tRun.sortRun();
+			tRun.clearPages();
+		}
+		
+		tRun.AddPage(tPage);
+		tRun.sortRun();
+		// TODO:: write run to file
+		tPage.EmptyItOut();
+	}
+	else if(tRun.getRunSize()!=0) {
+		tRun.sortRun();
+		// TODO:: write run to file
+		tRun.clearPages();
+	}
+
 
     // construct priority queue over sorted runs and dump sorted data 
- 	// into the out pipe
+	
+	// insert into the out pipe
 
     // finally shut down the out pipe
 	out.ShutDown ();
@@ -12,3 +49,28 @@ BigQ :: BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen) {
 
 BigQ::~BigQ () {
 }
+
+// ------------------------------------------
+Run::Run(int runlen) {
+	this->runLength = runlen;
+}
+void Run::AddPage(Page p) {
+	this->pages.push_back(p);
+}
+void Run::sortRun() {
+	// sort pages
+	// insert pages into tournament
+}
+vector<Page> Run::getPages() {
+	return this->pages;
+}
+bool Run::checkRunFull() {
+	return this->pages.size() == this->runLength;
+}
+bool Run::clearPages() {
+	this->pages.clear();
+}
+int Run::getRunSize() {
+	return this->pages.size();
+}
+//------------------------------------------
--- BigQ.h
+++ BigQ.h
@@ -2,18 +2,34 @@
 #define BIGQ_H
 #include <pthread.h>
 #include <iostream>
+#include <vector>
 #include "Pipe.h"
 #include "File.h"
 #include "Record.h"
-
 using namespace std;
 
+// ------------------------------------------------------------------
 class BigQ {
-
 public:
-
 	BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen);
 	~BigQ ();
 };
+// ------------------------------------------------------------------
+
+
+// ------------------------------------------------------------------
+class Run {
+	int runLength;
+	vector <Page> pages;
+	public:
+		Run(int runLength);
+		void AddPage(Page p);
+		void sortRun();
+		bool checkRunFull();
+		bool clearPages();
+		int getRunSize();
+		vector<Page> getPages();
+};
+// ------------------------------------------------------------------
 
 #endif
